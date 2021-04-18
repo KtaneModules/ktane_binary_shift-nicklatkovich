3e70a9e5cdeace4ac541f008fafdf7465225b840
NickLatkovich <nicklatkovich@gmail.com>
2021-04-19 00:00:00 +0300

    Init

---
+++ Assets/BinaryShift/Scripts/BinaryShiftModule.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BinaryShiftModule : MonoBehaviour {
+	public Transform NumberScreensContainer;
+	public KMBombInfo BombInfo;
+	public KMBombModule BombModule;
+	public KMSelectable SelfSelectable;
+	public NumberComponent NumberComponentPrefab;
+
+	private NumberComponent[] numbers;
+
+	void Start() {
+		List<NumberComponent> numbers = new List<NumberComponent>();
+		for (int i = -1; i < 2; i++) {
+			for (int j = 0; j < 3; j++) {
+				NumberComponent number = Instantiate(NumberComponentPrefab);
+				number.transform.parent = NumberScreensContainer;
+				number.transform.localPosition = new Vector3(0.05f * i, 0f, -0.03f * j);
+				number.transform.localScale = Vector3.one;
+				number.transform.localRotation = Quaternion.identity;
+				number.Selectable.Parent = SelfSelectable;
+				number.target = Random.Range(0, 100);
+				numbers.Add(number);
+			}
+		}
+		this.numbers = numbers.ToArray();
+		SelfSelectable.Children = numbers.Select((n) => n.Selectable).ToArray();
+		SelfSelectable.UpdateChildren();
+	}
+}
---
+++ Assets/BinaryShift/Scripts/NumberComponent.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class NumberComponent : MonoBehaviour {
+	private const float SPEED = 10f;
+
+	public TextMesh Text;
+	public Renderer Display;
+	public KMSelectable Selectable;
+
+	public int target = 0;
+
+	private bool _highlighted = false;
+	public bool highlighted {
+		get { return _highlighted; }
+		private set {
+			if (_highlighted == value) return;
+			_highlighted = value;
+			UpdateColors();
+		}
+	}
+
+	private bool _selected = false;
+	public bool selected {
+		get { return _selected; }
+		private set {
+			if (_selected == value) return;
+			_selected = value;
+			Display.material.SetColor("_Color", selected ? Color.white : Color.black);
+			UpdateColors();
+		}
+	}
+
+	private int _value = 0;
+	public int value {
+		get { return _value; }
+		private set {
+			if (_value == value) return;
+			_value = value;
+			Text.text = value.ToString();
+		}
+	}
+
+	private float delta = 0f;
+
+	private void Start() {
+		Selectable.OnHighlight += () => highlighted = true;
+		Selectable.OnHighlightEnded += () => highlighted = false;
+		Selectable.OnInteract += () => { selected = !selected; return false; };
+		value = 0;
+	}
+
+	private void Update() {
+		delta += Time.deltaTime;
+		int steps = Mathf.FloorToInt(delta * SPEED);
+		if (steps == 0) return;
+		delta -= steps / SPEED;
+		if (Mathf.Abs(target - value) <= steps) value = target;
+		else value += steps * (int)Mathf.Sign(target - value);
+	}
+
+	private IEnumerator UpdateValue() {
+		yield return new WaitForSeconds(Random.Range(0f, .1f));
+		while (true) {
+			if (target < 0) value = Random.Range(0, 100);
+			yield return new WaitForSeconds(.1f);
+		}
+	}
+
+	private void UpdateColors() {
+		Text.color = highlighted ? Color.red : (selected ? Color.black : Color.white);
+	}
+}
